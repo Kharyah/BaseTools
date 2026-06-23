447f5bc01ca324c6ddbc33279b47cc64a11977f2
Kharyah <206756988+Kharyah@users.noreply.github.com>
2026-06-23 01:30:08 -0300

    feat: add main project files, config, requirements and gitignore

---
+++ src/config.py
@@ -0,0 +1,50 @@
+import json
+from pathlib import Path
+
+# -------------------------------------------------------------------------
+# If you cloned this repo, do yourself a favor and go listen to 
+# "Deixa escondido" (sped up or original) by Vitux.
+# Honestly? The lyrics might be a bit "meh" or questionable, but the 
+# beat and the vibe are absolute gold. Just vibe with it, don't overthink it.
+# -------------------------------------------------------------------------
+
+JSON_FILE = Path("src/folders_path.json")
+
+def create_folders_path():
+    global JSON_FILE
+
+    if not JSON_FILE.exists():
+        default_paths = {
+            "IMAGE_PATH": [
+                str(Path.home() / "Pictures"),
+            ],
+            "DOWNLOAD_PATH": [
+                str(Path.home() / "Downloads"),
+            ],
+            "STR_PATH": [
+                str(Path.home() / "Documents")
+            ]
+        }
+
+        with open(JSON_FILE, "w", encoding="utf-8") as f:
+            json.dump(default_paths, f, indent=4)
+    return None
+
+def read_path(path_name: str = None):
+    with open(JSON_FILE, "r", encoding="utf-8") as f:
+        dates = json.load(f)
+
+    return dates[path_name] if path_name != None else dates
+
+def update_path(path_name, new_path):
+    path_append = read_path()    
+    new_path = str(new_path)
+
+    if new_path not in path_append[path_name]:
+        if len(path_append[path_name]) <= 3:
+            path_append[path_name].append(new_path)
+        else:
+            path_append[path_name][0] = new_path
+
+    with open(JSON_FILE, "w", encoding="utf-8") as f:
+        json.dump(path_append, f, indent=4)
---
+++ src/main.py
@@ -0,0 +1,101 @@
+import os
+
+from InquirerPy import inquirer
+from InquirerPy.base.control import Choice
+from pathlib import Path
+
+# TODO - Remember to call functions from converter, downloader and generate
+from config import create_folders_path, read_path, update_path
+
+def file_path_choice():
+    os.system("cls" if os.name == "nt" else "clear")
+    print("--- Choice Image ---")
+
+    current_path_directory = os.path.expanduser("~")
+
+    paths = read_path("IMAGE_PATH")
+    paths_list = [Choice(value=x, name=x) for x in paths]
+    paths_list.append(Choice(value="Another", name="> Choice another path."))
+
+    default_path = inquirer.select(
+        message="Choice a Default Path!",
+        choices=paths_list
+    ).execute()
+
+    if default_path == "Another":
+        while True:
+            file_path = inquirer.filepath(
+                message="Choice the Image File!",
+                default=current_path_directory,
+            ).execute()
+
+            if not file_path:
+                break
+            
+            if os.path.isfile(file_path):
+                return file_path
+
+            elif os.path.isdir(file_path):
+                current_path_directory = file_path
+                os.system("cls" if os.name == "nt" else "clear")
+    return default_path
+
+def media_downloader():
+    pass
+
+def image_converter():
+    print("\n--- Image Converter ---")
+    file_path = file_path_choice()
+
+    types_list = ["PNG", "JPEG", "WEBP"]
+    file_format_list = [Choice(value=x, name=x) for x in types_list]
+    file_format_list.append(Choice(value="Return", name="Return"))
+
+    file_format = inquirer.select(
+        message="Choice The File Format:",
+        choices=file_format_list
+    ).execute()
+
+    if file_format == "Return":
+        os.system("cls" if os.name == "nt" else "clear")
+    else:        
+        new_path = Path(file_path)
+        update_path(path_name="IMAGE_PATH", new_path=new_path.parent)
+
+        # TODO - Create the system to convert the image and save it in the right folder. 
+        # TODO - Maybe think about configuring to save to the same path or if the user chooses another path.
+
+def srt_generator():
+    pass
+
+def main():
+    call_functions = {
+        "Media Downloader": media_downloader,
+        "Image Converter": image_converter,
+        "Srt Generator": srt_generator
+    }
+
+    choices_values = ["Media Downloader", "Image Converter", "Srt Generator", "Exit"]
+    choices_texts = ["1. Downloader Media with yt-dlp.", "2. Converter Images.", "3. Generator srt files.", "4. Exit"]
+    choices_list = [Choice(value=x, name=y) for x, y in zip(choices_values, choices_texts)]
+
+    while True:
+        user_choice = inquirer.select(
+            message="Select Tool!",
+            choices=choices_list,
+            default="Media Downloader"
+        ).execute()
+
+        if user_choice in call_functions.keys():
+            call_functions[user_choice]()
+        else:
+            print("Closing...")
+            break
+
+        os.system("cls" if os.name == "nt" else "clear")
+
+if __name__ == "__main__":
+    os.system("cls" if os.name == "nt" else "clear")
+    create_folders_path() # Criando o arquivo json
+
+    main()
